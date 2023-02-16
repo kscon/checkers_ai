e9fd96e5e870b15ee2e7511571dc58cc27a663ca
Kevin Conrads <kevin.conrads@rwth-aachen.de>
2023-02-16 20:15:41 +0100

    begin of is valid move method

--- GameDriver.py
+++ GameDriver.py
@@ -21,10 +21,12 @@ class GameDriver:
             return -1
 
         if self.is_valid_move(source_col, source_row, target_col, target_row):
+            # todo
             pass
 
     # todo
-    # expects valid source and target field (i.e., on the board), rest is done here
+    # expects valid source and target field (i.e., on the board),
+    # validation of the move is done here
     def is_valid_move(self, source_row, source_col, target_row, target_col):
         piece = self.game_board.get_field(source_col, source_row).get_Piece()
         current_player = self.current_player
@@ -33,47 +35,44 @@ class GameDriver:
         checked_piece = ' '
 
         # todo
-        # normal moves
-        # normal move left and right
-        if abs(col_index_source - col_index_target) == 1:
-            # normal move forward
-            if target_row - source_row == 1:
+        # pawn moves
+        if piece.get_piece_type() == 'pawn':
+            # normal moves
+            # normal move left and right
+            if abs(col_index_source - col_index_target) == 1:
                 target_piece = self.game_board.get_field(target_col, target_row)
-                if current_player == 0 and target_piece == ' ' or \
-                        current_player == 1 and piece == 'B':
+                # normal move white
+                if target_row - source_row == 1 and current_player == 0 and target_piece is None:
                     return 1
-            # normal move backwards
-            if target_row - source_row == -1:
-                target_piece = self.game_board.get_field(target_col, target_row)
-                if current_player == 1 and target_piece == ' ' or \
-                        current_player == 0 and piece == 'W':
+                # normal move black
+                elif target_row - source_row == -1 and current_player == 1 and target_piece == ' ':
                     return 1
-        # checking moves
-        # checking move left
-        elif self.game_board.cols.index(source_col) - self.game_board.cols.index(target_col) == -2:
-            # checking move left forward
-            if target_row - source_row == 2:
-                checked_piece=self.game_board.get_field(
-                    self.game_board.cols[col_index_source+1],source_row+1).get_piece()
-                if checked_piece == 'b' or checked_piece == 'B':
-                    return True
-            # checking move left backwards
-            elif target_row - source_row == -2:
-                checked_piece=self.game_board.get_field(
-                    self.game_board.cols[col_index_source+1],source_row-1).get_piece()
-                if checked_piece == 'b' or checked_piece == 'B':
-                    return True
-        # checking move right
-        elif self.game_board.cols.index(source_col) - self.game_board.cols.index(target_col) == 2:
-            # checking move right forward
-            if target_row - source_row == 2:
-                checked_piece=self.game_board.get_field(
-                    self.game_board.cols[col_index_source-1],source_row+1).get_piece()
-                if checked_piece == 'b' or checked_piece == 'B':
-                    return True
-            # checking move right backwards
-            elif target_row - source_row == -2:
-                checked_piece=self.game_board.get_field(
-                    self.game_board.cols[col_index_source-1],source_row-1).get_piece()
-                if checked_piece == 'b' or checked_piece == 'B':
-                    return True
+            # checking moves
+            # checking move left
+            elif self.game_board.cols.index(source_col) - self.game_board.cols.index(target_col) == -2:
+                # checking move left forward
+                if target_row - source_row == 2:
+                    checked_piece = self.game_board.get_field(
+                        self.game_board.cols[col_index_source + 1], source_row + 1).get_piece()
+                    if checked_piece == 'b' or checked_piece == 'B':
+                        return True
+                # checking move left backwards
+                elif target_row - source_row == -2:
+                    checked_piece = self.game_board.get_field(
+                        self.game_board.cols[col_index_source + 1], source_row - 1).get_piece()
+                    if checked_piece == 'b' or checked_piece == 'B':
+                        return True
+            # checking move right
+            elif self.game_board.cols.index(source_col) - self.game_board.cols.index(target_col) == 2:
+                # checking move right forward
+                if target_row - source_row == 2:
+                    checked_piece = self.game_board.get_field(
+                        self.game_board.cols[col_index_source - 1], source_row + 1).get_piece()
+                    if checked_piece == 'b' or checked_piece == 'B':
+                        return True
+                # checking move right backwards
+                elif target_row - source_row == -2:
+                    checked_piece = self.game_board.get_field(
+                        self.game_board.cols[col_index_source - 1], source_row - 1).get_piece()
+                    if checked_piece == 'b' or checked_piece == 'B':
+                        return True
