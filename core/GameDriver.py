6da8fc774e4b1b4ae49221679f354f45dcf8b5be
Kevin Conrads <kevin.conrads@rwth-aachen.de>
2023-04-01 22:47:22 +0200

    Moved is valid move method to board so that the ai can access it

--- core/GameDriver.py
+++ core/GameDriver.py
@@ -47,7 +47,7 @@ class GameDriver:
             print('Not a valid field/notation!')
             return -1
 
-        res = self.is_valid_move(source_col, source_row, target_col, target_row)
+        res = self.game_board.is_valid_move(self.current_player, source_col, source_row, target_col, target_row)
         if res[0] == 1:
             self.game_board.move_piece(source_col, source_row, target_col, target_row)
             if res[1] is not None:
@@ -62,76 +62,12 @@ class GameDriver:
     # returns: (code, field), where field depicts a checked piece or None, and code
     # -1 for an invalid move
     # 1 for a vaild move
-    def is_valid_move(self, source_col, source_row, target_col, target_row):
-        piece = self.game_board.get_field(source_col, source_row).get_Piece()
-        current_player = self.current_player
-        col_index_source = self.game_board.cols.index(source_col)
-        row_index_source = self.game_board.rows.index(source_row)
-        col_index_target = self.game_board.cols.index(target_col)
-        row_index_target = self.game_board.rows.index(target_row)
-        checked_field = ' '
-
-        play_direction = 1  # 1 for white, -1 for black
-        if self.current_player == 1:
-            play_direction = -1
-
-        # pawn moves
-        if piece.get_piece_type() == 'pawn':
-            # normal moves
-            if abs(col_index_source - col_index_target) == 1:
-                target_field = self.game_board.get_field(target_col, target_row)
-                if target_row - source_row == play_direction and target_field.get_Piece() is None:
-                    return 1, None
-                """# normal move white
-                if target_row - source_row == 1 and current_player == 0 and target_piece is None:
-                    return 1
-                # normal move black
-                elif target_row - source_row == -1 and current_player == 1 and target_piece == ' ':
-                    return 1"""
-
-            # checking moves
-            elif abs(col_index_source - col_index_target) == 2:
-                # checking move left forward
-                target_field = self.game_board.get_field(target_col, target_row)
-                if target_row - source_row == 2 * play_direction and \
-                        target_field.get_Piece() is None:
-                    checked_field = self.game_board.get_field_by_index(
-                        col_index_source + (col_index_target - col_index_source) - 1,
-                        row_index_source + play_direction)
-                    if checked_field.get_Piece() is None:
-                        return -1, None
-                    if checked_field.get_Piece().player_color == 'white' and current_player == 1:
-                        return 1, checked_field
-                    elif checked_field.get_Piece().player_color == 'black' and current_player == 0:
-                        return 1, checked_field
-            """# checking move left backwards
-                        elif target_row - source_row == -2:
-                            checked_piece = self.game_board.get_field(
-                                self.game_board.cols[col_index_source + 1], source_row - 1).get_piece()
-                            if checked_piece == 'b' or checked_piece == 'B':
-                                return True
-                        # checking move right
-                        elif self.game_board.cols.index(source_col) - self.game_board.cols.index(target_col) == 2:
-                            # checking move right forward
-                            if target_row - source_row == 2:
-                                checked_piece = self.game_board.get_field(
-                                    self.game_board.cols[col_index_source - 1], source_row + 1).get_piece()
-                                if checked_piece == 'b' or checked_piece == 'B':
-                                    return True
-                            # checking move right backwards
-                            elif target_row - source_row == -2:
-                                checked_piece = self.game_board.get_field(
-                                    self.game_board.cols[col_index_source - 1], source_row - 1).get_piece()
-                                if checked_piece == 'b' or checked_piece == 'B':
-                                    return True"""
-            # move not valid
-        return -1, None
 
 
 gd = GameDriver()
-gd.game_loop()
+# gd.game_loop()
 # gd.print_board()
-"""gd.make_move('a3', 'b4')
+gd.make_move('a3', 'b4')
 gd.print_board()
 gd.make_move('b6', 'a5')
 gd.print_board()
@@ -139,6 +75,6 @@ gd.make_move('c3', 'd4')
 gd.print_board()
 gd.make_move('a5', 'c3')
 gd.print_board()
-gd.make_move('b2', 'c3')"""
+gd.make_move('b2', 'c3')
 
 # print(gd.is_valid_move('a', 3, 'b', 4))
--- test_gamedriver.py
+++ test_gamedriver.py
@@ -1,14 +1,15 @@
 import unittest
 
 from core.GameDriver import GameDriver
-from core.Board import Board
+from core.Board import Board, is_valid_move
+
 
 # todo: automate testing
 class GameDriverTestCase(unittest.TestCase):
     def test_move_validity_1(self):
         gd = GameDriver()
         # gd.print_board()
-        self.assertTrue(gd.is_valid_move('a', 3, 'b', 4))
+        self.assertTrue(is_valid_move(gd.game_board, gd.current_player, 'a', 3, 'b', 4))
         # self.assertEqual(True, False)  # add assertion here
 
 
