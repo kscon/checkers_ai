60320030901e0ee6b0d5d2b15a77caf909111d8d
Kevin Conrads <kevin.conrads@rwth-aachen.de>
2023-04-01 23:03:06 +0200

    started ai logic

--- core/Ai.py
+++ core/Ai.py
@@ -9,7 +9,21 @@ class Ai:
         return move
 
     def enumerate_moves(self, board):
-        pass
+        list_of_moves = []
+        play_direction = 1
+        if self.color == 1:
+            play_direction = -1
+
+        for col in board.cols:
+            for row in board.rows:
+                field = board.get_field(col, row)
+                if field.get_Piece() is not None:
+                    piece = field.get_Piece()
+                    if self.color == 0 and piece.player_color == 'white' or \
+                            self.color == 1 and piece.player_color == 'black':
+                        if piece.get_piece_type() == 'pawn':
+                            # enumerate possible moves
+                            target_col = ord(col) - 1
 
     def evaluate_moves(self):
         pass
